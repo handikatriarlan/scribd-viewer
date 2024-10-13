b8f5c42003f3249eb87a70ee058a584bcb5c6fdd
Arlann <handikaarlan@gmail.com>
2024-10-13 14:21:38 +0700

    chore: change web name

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Scribd Viewer",
+  description: "A simple web application to view Scribd documents by converting standard Scribd URLs into embeddable URLs. This application can be used to view pages on Scribd that are limited by premium access by converting the document URL into an embeddable format.",
   navItems: [
     {
       label: "Home",
